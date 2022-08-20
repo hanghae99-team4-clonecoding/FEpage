42cb63fb14d3642e9c3a67e4f6a114c69025d659
SuHwan,Kim <shkiimaa@naver.com>
2022-08-21 00:10:01 +0900

    fixed 수정

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -5,9 +5,6 @@ import styles from "../css_modules/addPost.module.css"
 const AddPost = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.fixed}>
-        <h2>홈</h2>
-      </div>
       <div className={styles.postWarp}>
         <div className={styles.addPostArea}>
           <div className={styles.profileImg}>
