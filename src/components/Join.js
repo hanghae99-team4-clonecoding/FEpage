f3f881a3fdc2b2de38b368b60e2eac1ab7feaeb8
Dopest-HYO <hyo01_@naver.com>
2022-08-25 18:46:47 +0900

    Access page, login, join CSS

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { addJoin } from "../redux/modules/joinSlice";
+import { FaTwitter, FaApple } from "react-icons/fa"
+import styles from "../css_modules/Join.module.css"
 
 function JoinModal({ joinPopup, openJoinModal }) {
   const dispatch = useDispatch();
@@ -24,67 +27,55 @@ function JoinModal({ joinPopup, openJoinModal }) {
   };
   console.log(joinData);
 
+  const navigate = useNavigate()
+
+
   return (
-    <div
-      style={{
-        background: "rgba(0,0,0,0.1)",
-        height: "1090px",
-        width: "1024px",
-        zIndex: 999,
-        position: "absolute",
-        top: 0,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div className="divCloseBtn">
-        <button className="closeBtn" onClick={openJoinModal}>
-          X
-        </button>
-      </div>
-      <form
-        onSubmit={onSubmitHandler}
-        style={{
-          background: "white",
-          height: "70vh",
-          width: "50vw",
-        }}
-      >
-        <div className="InputBox">
-          <p>가입하기</p>
-          <label>이메일</label>
-          <input
-            type="email"
-            name="email"
-            value={joinData.email}
-            onChange={onChangeHandler}
-            placeholder="이메일 주소를 입력하세요"
-          />
-          <label>패스워드</label>
-          <input
-            type="password"
-            name="password"
-            value={joinData.password}
-            onChange={onChangeHandler}
-            placeholder="비밀번호를 입력하세요"
-          />
-          <label>패스워드 확인</label>
-          <input
-            type="password"
-            name="passwordCheck"
-            value={joinData.passwordCheck}
-            onChange={onChangeHandler}
-            placeholder="비밀번호를 입력하세요"
-          />
-        </div>
-        <div className="divLoginBtn">
-          <button className="loginBtn" type="submit">
-            가입하기
+    <div className={styles.modalBg}> {/* 모달 배경 */}
+      <div className={styles.modal}> {/* 모달 사이즈 */}
+        <div className="divCloseBtn">
+          <button className="closeBtn" onClick={openJoinModal}>
+            X
           </button>
         </div>
-      </form>
-    </div>
+        <form className={styles.formWarp} onSubmit={onSubmitHandler} >
+          <div className={styles.inputBox}>
+            <h2>가입하기</h2>
+            <div className={styles.inputWarp}>
+              <label>이메일</label>
+              <input
+                type="email"
+                name="email"
+                value={joinData.email}
+                onChange={onChangeHandler}
+                placeholder="이메일 주소를 입력하세요"
+              />
+              <label>패스워드</label>
+              <input
+                type="password"
+                name="password"
+                value={joinData.password}
+                onChange={onChangeHandler}
+                placeholder="비밀번호를 입력하세요"
+              />
+              <label>패스워드 확인</label>
+              <input
+                type="password"
+                name="passwordCheck"
+                value={joinData.passwordCheck}
+                onChange={onChangeHandler}
+                placeholder="다시 한번 비밀번호를 입력하세요"
+              />
+              <div className="divLoginBtn">
+                <button className="loginBtn" type="submit" onClick={() => navigate('/')}>
+                  가입하기
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div >
   );
 }
 
--- src/pages/AccessPage.js
+++ src/pages/AccessPage.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { FaTwitter } from "react-icons/fa"
+import { FaTwitter, FaApple } from "react-icons/fa"
 import { FcGoogle } from "react-icons/fc"
 import LoginModal from "../components/Login"
 import JoinModal from "../components/Join"
 import { loginBgImg } from '../svg/login_bg.png';
+import styles from "../css_modules/accessPage.module.css"
 // import Layout from "../components/common/layout";
 // import Join from "../components/Join"
 
@@ -33,6 +34,7 @@ function AccessPage() {
       setJoinPopup(false)
     }
   }
+  // witdh 영역 2개 60 % 40퍼 주기
 
   const navigate = useNavigate()
 
@@ -40,118 +42,76 @@ function AccessPage() {
     <div style={{ display: "flex" }}  >
       {loginPopup === false ? <></> : <LoginModal loginPopup={loginPopup} openLoginModal={openLoginModal} />}
       {joinPopup === false ? <></> : <JoinModal joinPopup={joinPopup} openJoinModal={openJoinModal} />}
-      <img src='https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png' />
-      <Title>
-        <Titlecons>
-          <FaTwitter size="24" color="#0095ED" />
-        </Titlecons>
-        <Top>
-          <span>지금 일어나고 있는 일</span>
-        </Top>
 
-        <Middle>
-          <span>오늘 트위터에 가입하세요</span>
-        </Middle>
+      <div style={
+        {
+          width: "60%",
+          height: "100vh",
+          backgroundImage: "url(https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png)"
+        }
+      } >
+        <div style={
+          {
+            height: "100%",
+            margin: "0 auto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+          {/*왼쪽 twitter svg */}
+          <FaTwitter size="400" color="#FFFFFF" />
+        </div>
+        {/* <img
+          style={{
+            position: 'absolute',
+            top: "0",
+            left: "0",
+            height: "100%",
+            width: "100%"
+          }}
+          src='https://abs.twimg.com/sticky/illustrations/lohp_1302x955.png' /> */}
+      </div>
+      {/* -------------------------------------------- 오른쪽 시작--------------------------------------------------------- */}
 
-        <GoogleBtn>
-          <button style={{
-            width: '274px',
-            height: '38px',
-            margin: '-15px ',
-            backgroundColor: '#fff',
-            fontColor: 'black',
-            borderColor: '#eee'
-          }} onClick={() => navigate('/')}>
-            <Ic>
-              <FcGoogle size="24"></FcGoogle>
-            </Ic>
-            Google 계정으로 가입하기(구현X)
-          </button>
-        </GoogleBtn>
+      <div className={styles.rightContaner}>
+        <FaTwitter className={styles.logo} size="70" color="#0095ED" />
+        <h2>지금 일어나고 있는 일</h2>
 
-        <button style={{
-          width: '274px',
-          height: '38px',
-          margin: '15px',
-          backgroundColor: '#fff',
-          fontColor: 'black',
-          borderColor: '#eee'
-        }} onClick={() => navigate('/')}>
-          Apple에서 가입하기(구현X)
-        </button>
+        <div className={styles.mainWarp}>
+          <h3>오늘 트위터에 가입하세요.</h3>
+          <div className={styles.accessWarp}>
 
-        <p style={{ marginLeft: "120px" }}>또는</p>
+            <div className={styles.btnWarp}>
+              <button onClick={() => navigate('/')}>
+                <FcGoogle size="20"></FcGoogle>
+                Google 계정으로 가입하기
+              </button>
+            </div>
 
-        <button style={{
-          width: '274px',
-          height: '38px',
-          margin: '10px',
-          backgroundColor: '#0095ED',
-          fontColor: 'white',
-          borderColor: '#000'
-        }} onClick={openJoinModal}>이메일 주소로 가입하기</button>
+            <div className={`${styles.btnWarp} ${styles.marginBtn}`} >
+              <button onClick={() => navigate('/')}>
+                <FaApple size="20"></FaApple>
+                Apple에서 가입하기
+              </button>
+            </div>
 
-        <Agree>가입하시려면 쿠키 사용을 포함해 이용약관과 개인정보 처리방침에 동의해야 합니다.</Agree>
+            <p className={styles.Or}>또는</p>
 
-        <IsJoin>이미 트위터에 가입하셨나요?</IsJoin>
-        <button style={{
-          width: '274px',
-          height: '38px',
-          margin: '14px',
-          backgroundColor: '#fff',
-          fontColor: '#0095ED'
-        }} onClick={openLoginModal}> 로그인 </button>
+            <div className={`${styles.btnWarp} ${styles.marginBtn}`} >
+              <button onClick={openJoinModal}>이메일 주소로 가입하기</button>
+            </div>
 
-      </Title>
-    </div>
+            <p className={styles.terms}>가입하시려면 쿠키 사용을 포함해 이용약관과 개인정보 처리방침에 동의해야 합니다.</p>
+
+            <p className={styles.ruJoin}>이미 트위터에 가입하셨나요?</p>
+            <div div className={styles.btnWarp} >
+              <button onClick={openLoginModal}> 로그인 </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
   )
-
 }
 
 export default AccessPage;
-
-const Title = styled.div`
-  text-align: left;
-  margin: 150px 20px;
-  position: relative;
-`;
-const Logo = styled.img`
-  background-size: cover;
-  height: 100%;
-  width: 20px; 
-`;
-
-const Top = styled.div`
-  font-size: 3.2rem;
-  font-weight: bold;
-`;
-
-const Middle = styled.div`
-  margin: 30px 0;
-  font-weight: bold;
-  font-size: 2rem;
-`;
-const Agree = styled.p`
-  width: 240px;
-  height: 10px;
-  margin-left: 10px;
-  font-size: 3px;
-`;
-
-const IsJoin = styled.p`
-  margin-top: 40px`;
-
-const Ic = styled.div`
-  display:inline-block ;
-  position:relative;
-  bottom:15px;
-  left:0px;`
-
-const Titlecons = styled.div`
-  display: inline-block;
-  position:relative;
-  bottom:50px;`
-
-const GoogleBtn = styled.div`
-  margin-left:30px;`
-
